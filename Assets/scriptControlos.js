8786b5ca8f30fff5f36d0bb37796a0a82ed920f6
shutt <rmvmagalhaes@gmail.com>
2013-12-06 02:45:23 +0000

    tha line game prototipo

---
+++ Assets/scriptControlos.js
@@ -0,0 +1,40 @@
+﻿#pragma strict
+
+var velocidadeVertical : float;
+var velocidadeHorizontal : float;
+
+var ctrHoriz : String;
+var ctrVerti : String;
+
+private var v : float;
+private var h : float;
+
+function Start () {
+	
+
+}
+
+function Update () {
+	v = Input.GetAxis(ctrVerti);
+	h = Input.GetAxis(ctrHoriz);
+	
+	if(v){
+		rigidbody2D.velocity.y = v * velocidadeVertical;
+		if (Input.GetKey(KeyCode.LeftShift)){
+			rigidbody2D.velocity.y = v * (velocidadeVertical*2);
+		}
+	} else {
+		rigidbody2D.velocity.y = 0;
+	}
+	
+	if (h){
+		rigidbody2D.velocity.x = h * velocidadeHorizontal;
+		if (Input.GetKey(KeyCode.LeftShift)){
+			rigidbody2D.velocity.x = h * (velocidadeHorizontal*2);
+		}
+	} else {
+		rigidbody2D.velocity.x = 0;
+	}
+	
+	
+}
---
+++ Assets/scriptLinha.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+var origem : Transform;
+var destino : Transform;
+
+private var linha : LineRenderer;
+
+function Start () {
+	linha = GetComponent(LineRenderer);
+	linha.SetPosition(0,origem.position);
+	linha.SetPosition(1,destino.position);
+}
+
+function Update () {
+	if (origem.rigidbody2D.velocity != 0 || destino.rigidbody2D.velocity != 0){
+		linha.SetPosition(0,origem.position);
+		linha.SetPosition(1,destino.position);
+	}
+	
+	if (Physics2D.Linecast(origem.position,destino.position)){
+		Debug.Log("hit");
+	}
+}
